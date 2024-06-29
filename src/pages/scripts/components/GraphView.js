c41c4b49598d1db8b1e25cf4ed2e5415667c6158
lazergenixdev <lazergenixdev@gmail.com>
2024-06-29 02:19:45 -0700

    🧅 Implement remove nodes/edges

--- src/pages/scripts/components/GraphView.js
+++ src/pages/scripts/components/GraphView.js
@@ -1,6 +1,26 @@
 import { Graph } from "../Graph.js";
 import { draw_arrow } from "../graphics.js";
 
+function distance_sq(x0, y0, x1, y1) {
+    const dx = x1 - x0;
+    const dy = y1 - y0;
+    return dx * dx + dy * dy;
+}
+
+function distance_line_sq(ax, ay, bx, by, px, py) {
+    const pax = px - ax;
+    const pay = py - ay;
+    const bax = bx - ax;
+    const bay = by - ay;
+    const t = Math.max(Math.min((pax*bax + pay*bay) / (bax*bax + bay*bay), 1.0), 0.0);
+    const dx = pax - bax*t;
+    const dy = pay - bay*t;
+    return dx*dx + dy*dy;
+}
+
+const STIFF_MAX = 0.1;
+const STIFF_MIN = 0.0;
+
 class Physics {
     constructor(canvas, graph) {
         this.nodes = {};
@@ -25,8 +45,8 @@ class Physics {
         
         // Create nodes
         graph.nodes.forEach(name => {
-            const x = Math.random() * 100.0;
-            const y = Math.random() * 100.0;
+            const x = Math.random() * 100.0 + 10.0;
+            const y = Math.random() * 100.0 + 10.0;
             this.nodes[name] = Bodies.circle(x, y, this.nodeRadius, { restitution: 0.6, friction: 0.1, label: name })
             Matter.World.add(this.world, this.nodes[name]);
         });
@@ -37,7 +57,7 @@ class Physics {
                 Constraint.create({
                     bodyA: this.nodes[edge[0]], 
                     bodyB: this.nodes[edge[1]], 
-                    stiffness: 0.001,
+                    stiffness: STIFF_MAX,
                     length: 150
                 })
             );
@@ -46,7 +66,7 @@ class Physics {
 
         // Add mouse interaction
         const mouse = Matter.Mouse.create(canvas);
-        const mouseConstraint = Matter.MouseConstraint.create(
+        this.mouseConstraint = Matter.MouseConstraint.create(
             this.engine, {
                 mouse: mouse,
                 constraint: {
@@ -55,8 +75,8 @@ class Physics {
                 }
             }
         );
-        Matter.World.add(this.world, mouseConstraint);
-        
+        Matter.World.add(this.world, this.mouseConstraint);
+
         // Set gravity to attract towards the center of the screen
         this.world.gravity.x = 0;
         this.world.gravity.y = 0;
@@ -64,20 +84,98 @@ class Physics {
         // Run the engine
         Matter.Runner.run(this.runner, this.engine);
     }
+    
+    maxDistanceSq() { return this.nodeRadius * this.nodeRadius * 2.0; }
+
+    find_closest_node(x, y, maxDistanceSq = this.maxDistanceSq()) {
+        let minDistanceSq = 1e999;
+        let result = null;
+        for (const node of Object.values(this.nodes)) {
+            const ds = distance_sq(node.position.x, node.position.y, x, y);
+            if (ds < minDistanceSq) {
+                minDistanceSq = ds;
+                result = node;
+            }
+        }
+        return (minDistanceSq <= maxDistanceSq)? result : null;
+    }
+
+    find_closest_edge(x, y, maxDistanceSq = 100) {
+        let minDistanceSq = 1e999;
+        let result = null;
+        for (const edge of this.edges) {
+            const p0 = edge.bodyA.position;
+            const p1 = edge.bodyB.position;
+            const ds = distance_line_sq(p0.x, p0.y, p1.x, p1.y, x, y);
+            if (ds < minDistanceSq) {
+                minDistanceSq = ds;
+                result = edge;
+            }
+        }
+        return (minDistanceSq <= maxDistanceSq)? result : null;
+    }
+
+    remove_node(x, y, graph) {
+        const node = this.find_closest_node(x, y);
+        if (node !== null) {
+            const name = node.label;
+            graph.removeNode(name);
+
+            this.edges = this.edges.filter(edge => {
+                const keep = edge.bodyA.label !== name && edge.bodyB.label !== name;
+                if (!keep) Matter.World.remove(this.world, edge);
+                return keep;
+            });
+
+            Matter.World.remove(this.world, node);
+            delete this.nodes[name];
+        }
+    }
+
+    remove_edge(x, y, graph) {
+        const edge = this.find_closest_edge(x, y);
+        if (edge !== null) {
+            graph.removeEdge(edge.bodyA.label, edge.bodyB.label);
+            this.edges = this.edges.filter(e => e !== edge);
+            Matter.World.remove(this.world, edge);
+        }
+    }
 }
 
+const MODE_NORMAL      = 0;
+const MODE_NODE_ADD    = 1;
+const MODE_EDGE_ADD    = 2;
+const MODE_NODE_REMOVE = 3;
+const MODE_EDGE_REMOVE = 4;
+
 class GraphView extends HTMLElement {
+
     constructor() {
         super();
         // Attach a shadow root to the element.
         this.attachShadow({ mode: 'open' });
+        this.mode = MODE_NORMAL;
 
         const canvas = document.createElement('canvas');
         canvas.width = this.clientWidth;
         canvas.height = this.clientHeight;
+        canvas.addEventListener('click', event => {
+            const rect = canvas.getBoundingClientRect(); // Get element's position relative to viewport
+            const pos = [event.clientX - rect.left, event.clientY - rect.top];
+
+            switch (this.mode) {
+                case MODE_NODE_REMOVE:
+                    this.physics.remove_node(...pos, this.graph);
+                    break;
+                case MODE_EDGE_REMOVE:
+                    this.physics.remove_edge(...pos, this.graph);
+                    break;
+                default:
+            }
+        });
 
         this.graph = new Graph();
-        this.graph.test();
+        this.graph.test(); // TODO: REMOVE ME
 
         const script = document.createElement('script');
         script.src="https://cdnjs.cloudflare.com/ajax/libs/matter-js/0.17.1/matter.min.js";
@@ -110,11 +208,20 @@ class GraphView extends HTMLElement {
             ctx.fillRect(this.shadowRoot.x++, 0, 100, 100);
         }
 
+        let removedEdge = null;
+        if (this.mode === MODE_EDGE_REMOVE) {
+            const mousePosition = this.physics.mouseConstraint.constraint.pointA;
+            removedEdge = this.physics.find_closest_edge(mousePosition.x, mousePosition.y);
+        }
+
         // Render edges
         ctx.strokeStyle = 'white';
         ctx.fillStyle = 'white';
         ctx.lineWidth = 3;
         this.physics.edges.forEach(edge => {
+            const style = (removedEdge === edge)? 'red' : 'white';
+            ctx.strokeStyle = style;
+            ctx.fillStyle   = style;
             draw_arrow(
                 ctx,
                 edge.bodyA.position.x, edge.bodyA.position.y,
@@ -124,15 +231,22 @@ class GraphView extends HTMLElement {
             );
         });
         
+        ctx.strokeStyle = 'white';
         ctx.textAlign = "center";
         ctx.textBaseline = "middle";
+
+        let removedNode = null;
+        if (this.mode === MODE_NODE_REMOVE) {
+            const mousePosition = this.physics.mouseConstraint.constraint.pointA;
+            removedNode = this.physics.find_closest_node(mousePosition.x, mousePosition.y);
+        }
 
         // Render nodes
         for (const [name, node] of Object.entries(this.physics.nodes)) {
             ctx.beginPath();
             ctx.arc(node.position.x, node.position.y, this.physics.nodeRadius, 0, 2 * Math.PI);
             ctx.closePath();
-            ctx.fillStyle = 'blue';
+            ctx.fillStyle = (removedNode?.label === node.label)? 'red' : 'blue';
             ctx.fill();
             ctx.fillStyle = 'white';
             ctx.stroke();
@@ -144,19 +258,13 @@ class GraphView extends HTMLElement {
     }
 
     lockEdges() {
-        function distance(x0, y0, x1, y1) {
-            const dx = x1 - x0;
-            const dy = y1 - y0;
-            return Math.sqrt(dx * dx + dy * dy);
-        }
-        
         function nodeDistance(nodeA, nodeB) {
-            return distance(
+            return Math.sqrt(distance_sq(
                 nodeA.position.x,
                 nodeA.position.y,
                 nodeB.position.x,
                 nodeB.position.y,
-            );
+            ));
         }
 
         this.physics.edges.forEach(edge => {
--- src/pages/scripts/dijkstra.js
+++ src/pages/scripts/dijkstra.js
@@ -1,13 +1,18 @@
+const graphView = document.querySelector('graph-view');
 
 const stiffToggle = document.getElementById('stiff');
 stiffToggle.addEventListener('change', () => {
-    const graphView = document.querySelector('graph-view');
     if (stiffToggle.checked) {
         graphView.lockEdges();
     } else {
         graphView.unlockEdges();
     }
 });
+
+const mode = document.getElementById('mode');
+const setMode = () => graphView.mode = parseInt(mode.value);
+setMode();
+mode.addEventListener('change', setMode);
 
 //const edgeNode1 = document.getElementById('edge-node-1');
 //const edgeNode2 = document.getElementById('edge-node-2');
